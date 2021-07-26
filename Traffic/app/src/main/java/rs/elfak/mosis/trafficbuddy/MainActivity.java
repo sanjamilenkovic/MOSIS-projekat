17b7632bb205723dadc5707bc304d2f08aefc840
Sanja Milenkovic <sanceziska99@gmail.com>
2021-07-26 21:53:46 +0200

    init commit 2

---
+++ Traffic/app/src/main/java/rs/elfak/mosis/trafficbuddy/MainActivity.java
@@ -0,0 +1,14 @@
+package rs.elfak.mosis.trafficbuddy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
