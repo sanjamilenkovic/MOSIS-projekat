aee97898eaaaba9a43d6efc18a7fb04752bb91f7
Sanja Milenkovic <sanceziska99@gmail.com>
2021-07-31 16:50:15 +0200

    sign in, sign up fragments

--- Traffic/app/src/main/java/rs/elfak/mosis/trafficbuddy/MainActivity.java
+++ Traffic/app/src/main/java/rs/elfak/mosis/trafficbuddy/MainActivity.java
@@ -1,6 +1,9 @@
 package rs.elfak.mosis.trafficbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 
@@ -11,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void setFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.nav_host_fragment, fragment)
+                .commit();
+    }
+
 }
